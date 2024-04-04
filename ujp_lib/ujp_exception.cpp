9e861e8eb3b5b8bebc3b58b247409d825aa76e5b
pedresp <pedro.espinosa@alumnos.upm.es>
2024-04-04 18:06:27 +0200

    corrected and added a new exception

--- ujp_lib/ujp_exception.cpp
+++ ujp_lib/ujp_exception.cpp
@@ -1,13 +1,8 @@
-#ifndef __UJP__EXCEPTION__
-#define __UJP__EXCEPTION__
-
 #include "ujp_exception.hpp"
-#include <string>
-namespace ujp {
-class badformat_Exception : public std::exception {
-public:
-  badformat_Exception() : std::exception() {}
-};
-} // namespace ujp
 
-#endif
+namespace ujp {
+badformat_Exception::badformat_Exception() : std::exception() {}
+
+Key_Error::Key_Error() : message("Wrong key introduced, that key does not exist or has other type\n") {}
+const char *Key_Error::what() const noexcept { return message.c_str(); }
+} // namespace ujp
--- ujp_lib/ujp_exception.hpp
+++ ujp_lib/ujp_exception.hpp
@@ -2,11 +2,21 @@
 #define __UJP__EXCEPTION__
 
 #include <exception>
+#include <string>
 
 namespace ujp {
 class badformat_Exception : public std::exception {
 public:
-  badformat_Exception() {}
+  badformat_Exception();
+};
+
+class Key_Error : public std::exception {
+private:
+  std::string message;
+
+public:
+  Key_Error();
+  const char *what() const noexcept;
 };
 } // namespace ujp
 
